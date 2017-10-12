cd9d369ae9e5b9203bf5ff7f9dc8ec17f039aa3f
Kyle McDonald <kyle2mcdonald@gmail.com>
2017-10-12 16:27:14 -0400

    two 500 server errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  post 'welcome/checkout', to: 'welcome#checkout', as: 'checkout'
+  # post 'welcome/checkout', to: 'welcome#checkout', as: 'checkout'
 
   root 'welcome#index'
 
